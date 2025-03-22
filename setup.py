9b92c365a4a32d41a31535fd5d880aa151e1ed85
Pranav Satheesh <pranavsatheesh@login9.ufhpc>
2025-03-22 17:46:05 -0400

    Modified setup.py for a different version of the code and installed locally

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='illustris_python',
+    name='illustris_python_mod',
     version='1.0.0',
-    packages=["illustris_python"],
+    packages=["illustris_python_mod"],
     install_requires=["numpy", "h5py", "six"],
     tests_require=["nose","coverage"],
 )
